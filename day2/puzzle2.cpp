f9800726c223d8072458a43dfb578499ac2d42ea
momchiltues19 <mom.todorov.tues19@gmail.com>
2021-12-03 22:33:49 +0200

    day2 solutions

--- day2/puzzle2.cpp
+++ day2/puzzle2.cpp
@@ -3,16 +3,19 @@
 int main()
 {
 	std::string command;
-	int units = 0, horizontal_position = 0, depth = 0;
+	int units = 0, horizontal_position = 0, depth = 0, aim = 0;
 
 	while(std::cin >> command >> units)
 	{
 		if(command == "forward")
+		{
 			horizontal_position += units;
+			depth += aim * units;
+		}
 		if(command == "down")
-			depth += units;
+			aim += units;
 		if(command == "up")
-			depth -= units;
+			aim -= units;
 	}
 
 	std::cout << horizontal_position * depth << std::endl;
